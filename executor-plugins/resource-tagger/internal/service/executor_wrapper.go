70c5fbaba987b3af3e28d726059c427fb7a52ad9
0xff-dev <stevenshuang521@gmail.com>
2022-10-25 17:25:25 +0800

    feat: resource tagger support support more behavior

--- executor-plugins/resource-tagger/internal/service/executor_wrapper.go
+++ executor-plugins/resource-tagger/internal/service/executor_wrapper.go
@@ -46,10 +46,20 @@ func NewExecuteService() pb.ExecuteServer {
 	return new(ExecuteServiceImpl)
 }
 
+func (e *ExecuteServiceImpl) ExecuteBehavior(ctx context.Context, message *pb.ExecuteBehaviorMessage) (*pb.ExecuteBehaviorResponse, error) {
+	return &pb.ExecuteBehaviorResponse{
+		Behavior: []string{"label", "none"},
+	}, nil
+}
+
 func (e *ExecuteServiceImpl) Execute(ctx context.Context, message *pb.ExecuteMessage) (*pb.ExecuteResponse, error) {
 	klog.V(10).Infof("kubeconfig path: %s\n", *kubeconfig)
-	klog.V(4).Infof("ResourceName: %s, namespace: %s, exprval: %f, condval: %v, actionData: %v\n",
-		message.ResourceName, message.Namespace, message.ExprVal, message.CondVal, message.ActionData)
+	klog.V(4).Infof("ResourceName: %s, namespace: %s, exprval: %f, condval: %v, actionData: %v, behavior: %s\n",
+		message.ResourceName, message.Namespace, message.ExprVal, message.CondVal, message.ActionData, message.Behavior)
+
+	if message.Behavior == "none" || message.Behavior == "" {
+		return &pb.ExecuteResponse{Data: ""}, nil
+	}
 
 	resourceBaseFormat := fmt.Sprintf("%s/%s/%s:%s", message.Group, message.Version, message.Resources, message.ResourceName)
 	var (
